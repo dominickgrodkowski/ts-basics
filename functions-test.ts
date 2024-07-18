c74a8ac13515c9fe5e295d99537854a86edab6a0
Dominick Grodkowski <Dominick.Grodkowski@gmx.de>
2024-07-18 13:49:51 +0200

    feat: Add TypeScript functions and tests

---
+++ functions-test.ts
@@ -0,0 +1,6 @@
+import addNumbers, { getName } from "./functions";
+
+console.log(addNumbers(1, 2)); // 3
+console.log(addNumbers(3, 4)); // 7
+
+console.log(getName({ first: "Dominick", last: "Grodkowski"}));
