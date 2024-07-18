f7d1ea7e41643b909c4ca392233a8d9497b4fa8d
Dominick Grodkowski <Dominick.Grodkowski@gmx.de>
2024-07-18 12:17:57 +0200

    chore: Add TypeScript examples and interfaces

--- basics.ts
+++ basics.ts
@@ -8,3 +8,28 @@ console.log(userName);
 let myNumber: number = 10;
 let myRegex: RegExp = /foo/;
 const names: string[] = userName.split(' ');
+const myValues: Array<number> = [1, 2, 3];
+
+interface Person {
+    first: string;
+    last: string;
+}
+
+const myPerson: Person = {
+    first: 'John',
+    last: 'Grod'
+};
+const ids: Record<number, string> = {
+    10: 'a',
+    20: 'b'
+};
+
+if (ids[30]) {
+    console.log(ids[30]);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+[1, 2, 3].forEach(i => console.log(i));
+const output = [1, 2, 3].map(i => i * 2);
