c85ba85524302f7f8ac89d2046bf2b4517e8eaf0
Dominick Grodkowski <Dominick.Grodkowski@gmx.de>
2024-07-24 15:57:50 +0200

    chore: Refactor parseCoordinate function to support multiple argument types

--- parseCoordinate.ts
+++ parseCoordinate.ts
@@ -2,9 +2,35 @@ interface Coordinate {
     x: number;
     y: number;
     }
-    function parseCoordinate(obj: Coordinate): Coordinate {
-        return {
-            ...obj,
+    
+    function parseCoordinate(str: string): Coordinate;
+    function parseCoordinate(obj: Coordinate): Coordinate;
+    function parseCoordinate(x: number, y: number): Coordinate;
+    function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
+      let coord: Coordinate = {
+        x: 0,
+        y: 0,
+      };
+    
+      if (typeof arg1 === "string") {
+        (arg1 as string).split(",").forEach((str) => {
+          const [key, value] = str.split(":");
+          coord[key as "x" | "y"] = parseInt(value, 10);
+        });
+      } else if (typeof arg1 === "object") {
+        coord = {
+          ...(arg1 as Coordinate),
         };
+      } else {
+        coord = {
+          x: arg1 as number,
+          y: arg2 as number,
+        };
+      }
+    
+      return coord;
     }
-    function parseCoordinate2(obj: Coordinate): Coordinate {
+    
+    console.log(parseCoordinate(10, 20));
+    console.log(parseCoordinate({ x: 52, y: 35 }));
+    console.log(parseCoordinate("x:12,y:22"));
